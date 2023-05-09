09cae9a69bc98ccd5437cb014dcc8142f53377d2
GolovanovAlex <Golovanov.a.a@yandex.ru>
2023-05-09 19:22:48 +0300

    js

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
 const popup = document.querySelector('.popup');
 const skipBtn = document.querySelector('.popup__skip-btn');
 const visitBtn = document.querySelector('.popup__link');
+
+// ---- ---- time skip ---- ---- //
+let remainingTime = 5;
+let allowedToSkip = false;
+let popupTime;
+
+// ---- ---- popup add active ---- ---- //
+const showAd = () => {
+  if (window.scrollY > 100) {
+    popup.classList.add('active');
+    popupTime = setInterval(() => {
+      console.log(remainingTime);
+      skipBtn.innerHTML = `Skip in ${remainingTime}s`;
+      remainingTime--;
+      if (remainingTime < 0) {
+        allowedToSkip = true;
+        clearInterval(popupTime);
+        skipBtn.innerHTML = 'Skip';
+        skipBtn.classList.add('cursor');
+      }
+    }, 1000);
+  }
+};
+const startTimer = () => {
+  if (window.scrollY > 100) {
+    showAd();
+    window.removeEventListener('scroll', startTimer);
+  }
+};
+window.addEventListener('scroll', startTimer);
+
+// ---- ---- skipButton remove active ---- ---- //
+const skipAd = () => {
+  popup.classList.remove('active');
+};
+skipBtn.addEventListener('click', () => {
+  if (allowedToSkip) {
+    skipAd();
+  }
+});
