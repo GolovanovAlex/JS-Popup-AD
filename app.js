a87373aa5ffb4007b47bfd0f9c274d07c0602c97
GolovanovAlex <Golovanov.a.a@yandex.ru>
2023-05-09 20:22:59 +0300

    js cookie

--- app.js
+++ app.js
@@ -2,42 +2,51 @@ const popup = document.querySelector('.popup');
 const skipBtn = document.querySelector('.popup__skip-btn');
 const visitBtn = document.querySelector('.popup__link');
 
-// ---- ---- time skip ---- ---- //
 let remainingTime = 5;
 let allowedToSkip = false;
-let popupTime;
+let popupTimer;
 
-// ---- ---- popup add active ---- ---- //
-const showAd = () => {
-  if (window.scrollY > 100) {
-    popup.classList.add('active');
-    popupTime = setInterval(() => {
-      console.log(remainingTime);
-      skipBtn.innerHTML = `Skip in ${remainingTime}s`;
-      remainingTime--;
-      if (remainingTime < 0) {
-        allowedToSkip = true;
-        clearInterval(popupTime);
-        skipBtn.innerHTML = 'Skip';
-        skipBtn.classList.add('cursor');
-      }
-    }, 1000);
-  }
+const createPopupCookie = () => {
+  let expiresDays = 30;
+  let date = new Date();
+  date.setTime(date.getTime() + expiresDays * 24 * 60 * 60 * 1000);
+  let expires = 'expires=' + date.toUTCString();
+  document.cookie = `popupCookie=true; ${expires}; path=/;`;
 };
+
+const showAd = () => {
+  popup.classList.add('active');
+  popupTimer = setInterval(() => {
+    skipBtn.innerHTML = `Skip in ${remainingTime}s`;
+    remainingTime--;
+
+    if (remainingTime < 0) {
+      allowedToSkip = true;
+      skipBtn.innerHTML = 'Skip';
+      skipBtn.classList.add('cursor');
+      clearInterval(popupTimer);
+    }
+  }, 1000);
+};
+
+const skipAd = () => {
+  popup.classList.remove('active');
+  createPopupCookie();
+};
+
+skipBtn.addEventListener('click', () => {
+  if (allowedToSkip) {
+    skipAd();
+  }
+});
+
 const startTimer = () => {
   if (window.scrollY > 100) {
     showAd();
     window.removeEventListener('scroll', startTimer);
   }
 };
-window.addEventListener('scroll', startTimer);
 
-// ---- ---- skipButton remove active ---- ---- //
-const skipAd = () => {
-  popup.classList.remove('active');
-};
-skipBtn.addEventListener('click', () => {
-  if (allowedToSkip) {
-    skipAd();
-  }
-});
+if (!document.cookie.match(/^(.*;)?\s*popupCookie\s*=\s*[^;]+(.*)?$/)) {
+  window.addEventListener('scroll', startTimer);
+}
